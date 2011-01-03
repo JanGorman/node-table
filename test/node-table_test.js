54bb3aead6c8cb0cd04a174863294cfebfa17a56
Jan Gorman <gorman.jan@gmail.com>
2011-01-03 13:32:16 +0100

    proper column widths

--- test/node-table_test.js
+++ test/node-table_test.js
@@ -1,4 +1,4 @@
 var sys = require('sys'), table = require('table').table({ 'columnWidths': [10, 20] });
 
-table.setDefaultColumnAlignment(['left', 'center']).appendRow(['Node', 'Table']).appendRow(['second', 'row']);
+table.setDefaultColumnAlignment(['left', 'center']).appendRow(['Node', 'Table']).appendRow(['second', 'row']).appendRow(['third', 'row']);
 sys.puts(table.render());
