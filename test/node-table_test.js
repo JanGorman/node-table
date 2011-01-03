6dc8c93c0c484bd0fe912fcf8de6a57f4cf76c45
Jan Gorman <gorman.jan@gmail.com>
2011-01-03 12:34:37 +0100

    setter for default alignment

--- test/node-table_test.js
+++ test/node-table_test.js
@@ -1,4 +1,4 @@
 var sys = require('sys'), table = require('table').table({ 'columnWidths': [10, 20] });
 
-table.setDefaultColumnAlignment(['left', 'center']).appendRow(['Node', 'Table'])
+table.setDefaultColumnAlignment(['left', 'center']).appendRow(['Node', 'Table']).appendRow(['second', 'row']);
 sys.puts(table.render());
